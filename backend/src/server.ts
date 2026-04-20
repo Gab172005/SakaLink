0a7219538e1464397d6b8b8f71ea0d3b77b12a46
Desmond Perez <desmondrainierperez@gmail.com>
2026-04-20 13:55:43 +0800

    chore: removed unnecessary console testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,3 @@
-console.log("MONGO URI:", process.env.MONGODB_URI);
-console.log("RAW LENGTH:", process.env.MONGODB_URI?.length);
-console.log("RAW VALUE:", process.env.MONGODB_URI);
 import "dotenv/config";
 import express from 'express';
 import { connectDB } from "./config/mongoose.js";
