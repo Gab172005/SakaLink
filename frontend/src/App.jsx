aac8acc01ee3a89c992fe392fee528de05074859
AJRabaria <amrabaria@up.edu.ph>
2026-05-02 16:35:59 +0800

    feat: routed homepage to /browse

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,22 @@
-import './styles/global.css';
-import MarketplacePage from './components/MarketplacePage';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
-export default function App() {
-  return <MarketplacePage />;
+import './styles/global.css'
+import MarketplacePage from './components/MarketplacePage'
+
+function App() {
+  return (
+    <div className="app-container">
+      <Toaster position="top-center" reverseOrder={false} />
+      <main>
+        <Routes>
+          <Route path="/browse" element={<MarketplacePage />} />
+          <Route path="*" element={<div style={{ padding: '2rem' }}>Page Not Found</div>} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
+
+export default App
