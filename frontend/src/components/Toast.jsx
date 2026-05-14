538f4ba60d4ba0034b3bf3ef353d43a993debadf
Gab172005 <lskalugdan@up.edu.ph>
2026-05-14 22:49:51 +0800

    chore: Clean up duplicate components and fix structural redundancy

--- frontend/src/components/Toast.jsx
+++
@@ -1,9 +0,0 @@
-import "./Toast.css";
-
-export default function Toast({ visible, message }) {
-  return (
-    <div className={`toast ${visible ? "show" : ""}`}>
-      {message}
-    </div>
-  );
-}
